b45f66ef89a0415018b9633ff7eefdc0db8a49a9
Maaz <maazmuhammad9266@gmail.com>
2021-08-16 21:28:52 +0500

    icons added

--- components/homepageComponents/ideaSection.js
+++ components/homepageComponents/ideaSection.js
@@ -4,22 +4,28 @@ const IdeaSection = () => {
   return (
     <div className="bg-[#FCF6CC] w-[100%] flex flex-col justify-center items-center pt-[100px] pb-[30px]">
       <div className=" w-[90%]">
-        <img
-          src="/images/large-image-person-phone.jpg"
-          width={"100%"}
-        />
+        <img src="/images/large-image-person-phone.jpg" width={"100%"} />
       </div>
       <h2 className="font-bold text-[40px] pb-[40px] pt-[40px]">Why</h2>
       <div className="flex flex-row justify-center items-center w-[100%]">
-        <div className="w-[30%]">
+        <div className="flex flex-col justify-center items-center w-[30%] ">
+          <div className="flex justify-center items-center rounded-full mb-8 bg-white w-[100px] h-[100px]">
+            <img src="/images/cv-icon.png" width={"80px"} />
+          </div>
           <p className="font-medium text-center text-[18px]">Beyond your CV</p>
         </div>
-        <div className="w-[30%]">
+        <div className="flex flex-col justify-center items-center w-[30%] ">
+          <div className="flex justify-center items-center rounded-full mb-8 bg-white w-[100px] h-[100px]">
+            <img src="/images/cv-icon.png" width={"80px"} />
+          </div>
           <p className="font-medium text-center text-[18px]">
             Meet other professionals
           </p>
         </div>
-        <div className="w-[30%]">
+        <div className="flex flex-col justify-center items-center w-[30%] ">
+          <div className="flex justify-center items-center rounded-full mb-8 bg-white w-[100px] h-[100px]">
+            <img src="/images/cv-icon.png" width={"80px"} />
+          </div>
           <p className="font-medium text-center text-[18px]">
             Discover talents around you
           </p>
